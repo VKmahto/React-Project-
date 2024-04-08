0b71c85db7dfad01e1c00b7133d7be3d3731e894
VKmahto <lucktvivekmehta@gmail.com>
2024-04-08 17:14:15 +0530

    Todo With Localstorage

---
+++ 10todocontext/src/context/TodoContext.js
@@ -0,0 +1,29 @@
+import { createContext, useContext } from "react";
+
+export const TodoContext = createContext({
+    todos : [
+        {
+            id : 1,
+            todo : "Todo Message",
+            completed : false,
+        }
+    ],
+    addTodo : (todo) =>{
+
+    },
+    updateTodo: (id, todo) =>{
+
+    },
+    deleteTodo: (id) =>{
+
+    },
+    toggelComplete:(id)=>{
+        
+    }
+})
+
+export const useTodo = ()=>{
+    return useContext(TodoContext)
+}
+
+export const TodoProvider =TodoContext.Provider
